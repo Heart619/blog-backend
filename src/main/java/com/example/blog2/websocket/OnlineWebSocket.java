bc7fa12a9db147ed18817bac9b3ecfa0e054feae
Heart619 <2508615597@qq.com>
2023-02-07 21:37:27 +0800

    细节改动

--- src/main/java/com/example/blog2/websocket/OnlineWebSocket.java
+++ src/main/java/com/example/blog2/websocket/OnlineWebSocket.java
@@ -1,16 +1,11 @@
 package com.example.blog2.websocket;
 
-import com.example.blog2.utils.LocationUtil;
-import com.example.blog2.vo.UserLocationVo;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.net.InetSocketAddress;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
@@ -32,16 +27,9 @@ public class OnlineWebSocket {
 
     @OnOpen
     public void onConnection(Session session) throws IOException {
-        String tmpIp = getRemoteAddress(session).getHostString();
         this.session = session;
-
-        UserLocationVo userLocation = LocationUtil.getUserLocation(tmpIp);
-        UserLocationVo.Result result = userLocation.getResult();
-        this.ip = result.getIp();
         if (!SESSION_ID_TO_WEBSOCKET.containsKey(this.ip)) {
             ONLINE_USER.incrementAndGet();
-            UserLocationVo.Ad_info ad_info = result.getAd_info();
-            log.info("~Welcome~ IP：[{}] 位于[{}-{}-{}-{}]访问", this.ip, ad_info.getNation(), ad_info.getProvince(), ad_info.getCity(), ad_info.getDistrict());
         }
         SESSION_ID_TO_WEBSOCKET.put(this.ip, this);
         sendOnlineTotal();
@@ -67,46 +55,6 @@ public class OnlineWebSocket {
 
     @OnError
     public void onError(Throwable error) {
-    }
-
-    public static InetSocketAddress getRemoteAddress(Session session) {
-        if (session == null) {
-            return null;
-        }
-        RemoteEndpoint.Async async = session.getAsyncRemote();
-        //在Tomcat 8.0.x版本有效
-//        InetSocketAddress addr0 = (InetSocketAddress) getFieldInstance(async,"base#sos#socketWrapper#socket#sc#remoteAddress");
-//        System.out.println("clientIP0" + addr0);
-        //在Tomcat 8.5以上版本有效
-        InetSocketAddress addr = (InetSocketAddress) getFieldInstance(async, "base#socketWrapper#socket#sc#remoteAddress");
-        return addr;
-    }
-
-
-    private static Object getFieldInstance(Object obj, String fieldPath) {
-        String fields[] = fieldPath.split("#");
-        for (String field : fields) {
-            obj = getField(obj, obj.getClass(), field);
-            if (obj == null) {
-                return null;
-            }
-        }
-
-        return obj;
-    }
-
-    private static Object getField(Object obj, Class<?> clazz, String fieldName) {
-        for (; clazz != Object.class; clazz = clazz.getSuperclass()) {
-            try {
-                Field field;
-                field = clazz.getDeclaredField(fieldName);
-                field.setAccessible(true);
-                return field.get(obj);
-            } catch (Exception e) {
-            }
-        }
-
-        return null;
     }
 
 }
