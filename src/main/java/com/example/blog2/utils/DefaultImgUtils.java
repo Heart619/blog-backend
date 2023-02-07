aab0adca1c10ea953b16fd9fee90db8ffab01676
Heart619 <2508615597@qq.com>
2023-02-07 22:44:05 +0800

    默认图片生成规则修改

--- src/main/java/com/example/blog2/utils/DefaultImgUtils.java
+++ src/main/java/com/example/blog2/utils/DefaultImgUtils.java
@@ -8,16 +8,19 @@ import org.springframework.util.StringUtils;
  */
 public class DefaultImgUtils {
 
-    private static final String DEFAULT_IMG_1 = "default/other0.jpg";
-
-    private static final String DEFAULT_IMG_2 = "default/other1.jpg";
+    private static final String[] DEFAULT_IMG = new String[]{
+            "default/other0.jpg",
+            "default/other1.jpg",
+            "default/other2.jpg",
+            "default/other3.jpg",
+            "default/other4.jpg",
+            "default/other5.jpg",
+    };
 
     private static final String DEFAULT_AVATAR = "default/avatar.png";
 
-    private static final String IMG_COOKIE_NAME = "blog_cook_name";
-
     public static String getDefaultBackImg() {
-        return (System.currentTimeMillis() & 1) == 0 ? DEFAULT_IMG_1 : DEFAULT_IMG_2;
+        return DEFAULT_IMG[(int) (Math.random() * DEFAULT_IMG.length)];
     }
 
     public static String getDefaultAvatarImg() {
@@ -25,7 +28,12 @@ public class DefaultImgUtils {
     }
 
     public static boolean isDefaultBackImg(String img) {
-        return !StringUtils.isEmpty(img) && (DEFAULT_IMG_1.equals(img) || DEFAULT_IMG_2.equals(img));
+        for (String s : DEFAULT_IMG) {
+            if (s.equals(img)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static boolean isDefaultAvatarImg(String img) {
